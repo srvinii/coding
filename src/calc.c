8f461e8b14a3177de13bced62eab7043c005be48
iagor0ger <iagoroger1516@gmail.com>
2016-08-19 05:36:42 -0300

    Elevar um numero ao expoente definido pelo usuario

--- src/calc.c
+++ src/calc.c
@@ -8,6 +8,6 @@ int main()
     scanf("%d", &n1);
     printf("Informe o expoente: ");
     scanf("%d", &n2);
-    printf("%d elevado a %d = %1.f\n", n1, n2, (float)pow(n1, n2));
+    printf("%d elevado a %d = %1.f\n", n1, n2, (float)pow(n1, n2)); // casting
     return 0;
 }
