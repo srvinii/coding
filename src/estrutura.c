659b7e83eb4620bbb30be61e969b4f3a159ff79d
iagor0ger <iagoroger1516@gmail.com>
2016-08-23 14:44:25 -0300

    estrutura de dados

--- src/estrutura.c
+++ src/estrutura.c
@@ -10,13 +10,13 @@ struct nascimento {
 int main()
 {
 	struct nascimento dados;
-    printf("Informe o dia: ");
-    scanf("%d", &dados.dia);
-    printf("Informe o mes: ");
-    scanf("%d", &dados.mes);
-    printf("Informe o ano: ");
-    scanf("%d", &dados.ano);
-    printf("Data de nascimento: %d-%d-%d\n", dados.dia, dados.mes, dados.ano);
-    printf("Idade: %d\n", dados.ano - 2016);
+	printf("Informe o dia: ");
+	scanf("%d", &dados.dia);
+	printf("Informe o mes: ");
+	scanf("%d", &dados.mes);
+	printf("Informe o ano: ");
+	scanf("%d", &dados.ano);
+	printf("Data de nascimento: %d-%d-%d\n", dados.dia, dados.mes, dados.ano);
+	printf("Idade: %d\n", dados.ano - 2016);
     return 0;
 }
