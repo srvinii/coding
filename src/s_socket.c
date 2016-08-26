e0427b3a3f36e48bbe9a87f4e1a95472a30b5c73
Tatu Saw <iagoroger1516@gmail.com>
2016-08-25 22:50:25 -0300

    Update s_socket.c

--- src/s_socket.c
+++ src/s_socket.c
@@ -5,9 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
 
@@ -20,47 +18,48 @@ struct sockaddr_in rhost;
 
 int main()
 {
-	int msocket, csocket, slen, tm = sizeof(rhost);
-	char buffer[1024];
-	msocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(msocket == -1) {
-		perror("socket");
-		exit(1);
-	} else {
-		printf("socket criado com sucesso.\n");
-	}
+    int msocket, csocket, slen;
+    unsigned int tm = sizeof(rhost);
+    char buffer[1024];
+    msocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(msocket == -1) {
+        perror("socket");
+        exit(1);
+    } else {
+        printf("socket criado com sucesso.\n");
+    }
 
-	lhost.sin_family	= AF_INET;
-	lhost.sin_port	= htons(PORT);
-	memset(lhost.sin_zero, 0x0, 8);
+    lhost.sin_family	= AF_INET;
+    lhost.sin_port	= htons(PORT);
+    memset(lhost.sin_zero, 0, sizeof(lhost.sin_zero));
 
-	if(bind(msocket, (struct sockaddr*)&lhost, sizeof(lhost)) == -1) {
-		perror("bind");
-		exit(1);
-	}
+    if(bind(msocket, (struct sockaddr *) &lhost, sizeof(lhost)) == -1) {
+        perror("bind");
+        exit(1);
+    }
 
-	listen(msocket, LISTEN);
+    listen(msocket, LISTEN);
 
-	if((csocket = accept(msocket, (struct sockaddr*)&rhost, &tm)) == -1) {
-		perror("accept");
-		exit(1);
-	}
+    if((csocket = accept(msocket, (struct sockaddr *) &rhost, &tm)) == -1) {
+        perror("accept");
+        exit(1);
+    }
 
-	strcpy(buffer, "conectado ao servidor\n\0");
+    strcpy(buffer, "conectado ao servidor\n\0");
 
-	if(send(csocket, buffer, strlen(buffer), 0)) {
-		printf("aguardando resposta do cliente.\n");
-		while(1) {
-			memset(buffer, 0x0, tm);
-			if((slen = recv(csocket, buffer, LEN, 0)) > 0) {
-				buffer[slen] = '\0';
-				printf("Msg: %s", buffer);
-				close(csocket);
-				break;
-			}
-		}
-	}
-	close(msocket);
-	printf("server down.\n");
-	return 0;
+    if(send(csocket, buffer, strlen(buffer), 0)) {
+        printf("aguardando resposta do cliente.\n");
+        while(1) {
+            memset(buffer, 0x0, tm);
+            if((slen = recv(csocket, buffer, LEN, 0)) > 0) {
+                buffer[slen] = '\0';
+                printf("Msg: %s", buffer);
+                close(csocket);
+                break;
+            }
+        }
+    }
+    close(msocket);
+    printf("server down.\n");
+    return 0;
 }
