0da788f83eeaf7764c40ad6c5d5a28bd1d384b17
Iago Roger <iagoroger1516@gmail.com>
2016-09-04 09:20:36 -0300

    Sequência de Fibonacci em C++

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	long double num, calc, fib[2] = { 1, 1 };
+	std::cout << "Informe um numero: ";
+	std::cin >> num;
+	for(int c = 3; c <= num; c++) {
+		calc = fib[0] + fib[1];
+		fib[0] = fib[1];
+		fib[1] = calc;
+	}
+	std::cout << "Fibonacci: " << num << " = " << fib[1] << std::endl;
+	return 0;
+}
