d2ed8e64bb0f0988853909ccef0119949de81cfe
Iago Roger <iagoroger1516@gmail.com>
2016-09-22 22:07:33 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,22 @@
 // Ref: https://sites.google.com/a/liesenberg.biz/cjogos/home/materiais-de-apoio/topicos-relativos-a-c/recursao/serie-de-fibonacci
 //
 
+/// @file main.cpp
+
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
 	long double num, calc, fib[2] = { 1, 1 };
-	std::cout << "Informe um numero: ";
-	std::cin >> num;
-	for(int c = 3; c <= num; c++) {
+	cout << "Informe um numero: ";
+	cin >> num;
+	for (int c = 3; c <= num; c++) {
 		calc = fib[0] + fib[1];
 		fib[0] = fib[1];
 		fib[1] = calc;
 	}
-	std::cout << "Fibonacci: " << num << " = " << fib[1] << std::endl;
+	cout << "Fibonacci: " << num << " = " << fib[1] << endl;
 	return 0;
 }
