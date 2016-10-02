0940e42d1dfed2f46eed01a466cfb5c33745137b
iagor0ger <iagoroger1516@gmail.com>
2016-10-02 00:50:42 -0300

    added std::cin.get

--- src/tabuada.cpp
+++ src/tabuada.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
-    int a, b;
-    for (a = 1; a <= 10; ++a) {
-        for (b = 1; b <= 10; ++b) {
-            std::cout << a << " * " << b << " = " << a * b << std::endl;
+    int x, y;
+    for (x = 1; x <= 10; ++x) {
+        for (y = 1; y <= 10; ++y) {
+            std::cout << x << " * " << y << " = " << x * y << std::endl;
         }
         std::cout << "\n";
     }
+    std::cin.get(); // segura o cmd no windows
     return 0;
 }
