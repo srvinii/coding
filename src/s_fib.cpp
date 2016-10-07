1bbcc11e3c726f01fe592b3f54eea463114cf4d4
Iago Roger <iagoroger1516@gmail.com>
2016-10-07 07:05:00 -0300

    Update s_fib.cpp

--- src/s_fib.cpp
+++ src/s_fib.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
+int fib(int num);
+
 int main()
 {
-    int num, calc, fib[2] = { 1, 1 };
-    std::cout << "Informe um valor: ";
-    std::cin >> num;
-    for (int contador = 3; contador <= num; ++contador) {
-        calc = fib[0] + fib[1];
-        fib[0] = fib[1];
-        fib[1] = calc;
-    }
-    std::cout << "Fibonacci: " << fib[1] << std::endl;
-    return 0;
+	int n;
+	cout << "Informe um numero: ";
+	cin >> n;
+	cout << "Fibonacci: " << fib(n);
+	return 0;
+}
+
+int fib(int num)
+{
+	int calc, c, fib[2] = { 1, 1 };
+	for (c = 3; c <= num; ++c) {
+		calc = fib[0] + fib[1];
+		fib[0] = fib[1];
+		fib[1] = calc;
+	}
+	return fib[1];
 }
