371c971a22abc57f12f380b03eb2cebc742ba8e0
iagor0ger <iagoroger1516@gmail.com>
2016-10-12 11:14:50 -0300

    added ref

--- src/s_fib.cpp
+++ src/s_fib.cpp
@@ -1,25 +1,26 @@
-#include <iostream>
+/// Ref: https://oeis.org/A000045/b000045.txt
 
-using namespace std;
+#include <iostream>
 
 int fib(int num);
 
 int main()
 {
-	int n;
-	cout << "Informe um numero: ";
-	cin >> n;
-	cout << "Fibonacci de " << n << " = " << fib(n);
-	return 0;
+    int n;
+    std::cout << "Informe um numero: ";
+    std::cin >> n;
+    std::cout << "Fibonacci de " << n << " = " << fib(n) << std::endl;
+    return 0;
 }
 
 int fib(int num)
 {
-	int calc, c, fib[2] = { 1, 1 };
-	for (c = 3; c <= num; ++c) {
-		calc = fib[0] + fib[1];
-		fib[0] = fib[1];
-		fib[1] = calc;
-	}
-	return fib[1];
+    int calc, i, fib[2] = { 1, 1 };
+    for (i = 3; i <= num; ++i)
+    {
+        calc = fib[0] + fib[1];
+        fib[0] = fib[1];
+        fib[1] = calc;
+    }
+    return fib[1];
 }
